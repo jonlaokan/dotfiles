29d7279d9c974f3bdefad96c6e315f7dbe944533
jonlaokan <oneofdays@gmail.com>
2019-01-09 14:42:35 +0100

    feat(xmonad): update configuration
    Add a startup hook to call xmon, solve a problem for now that caused
    the wallpaper to not appear when fehbg is called.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -10,12 +10,12 @@ import XMonad.Util.Run
 
 import qualified Data.Map as M
 
-myTerminal    = "kitty"
+myTerminal    = "termite"
 myBorderWidth = 3
 myFocusedBorderColor = "#CAA9FA"
 myNormalBorderColor = "#BFBFBF"
 modm = mod4Mask
-myBar = "/usr/bin/xmobar"
+myBar = "xmobar"
 
 myPP = xmobarPP { ppCurrent = xmobarColor "#FF2266" "" . wrap "<" ">" }
 
@@ -28,6 +28,7 @@ myConfig = defaultConfig
         , normalBorderColor = myNormalBorderColor
 	, modMask = modm
 	, workspaces = myWorkspaces
+	, startupHook = myStartupHook
         , keys = \c -> mykeys c `M.union` keys defaultConfig c
         }
         where 
@@ -35,18 +36,18 @@ myConfig = defaultConfig
                 [ ((modm, xK_r), spawn "rofi-launch")
                 -- Volume controls
                 , ((0,xF86XK_AudioRaiseVolume), spawn
-                "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo +25%; notify_sound.sh -v" )
+                "ponymix increase 20" )
                 , ((0,xF86XK_AudioLowerVolume), spawn
-                "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo -25%; notify_sound.sh -v" )
+                "ponymix decrease 20" )
                 , ((0,xF86XK_AudioMute), spawn
-                "pactl set-sink-mute alsa_output.pci-0000_00_1b.0.analog-stereo toggle; notify_sound.sh -m" )
+                "ponymix toggle" )
                 -- Brightness controls
                 , ((0,xF86XK_MonBrightnessUp), spawn
-                "light -A 20")
-                , ((0,xF86XK_MonBrightnessDown), spawn "light -U 20")
+                "xbacklight +20")
+                , ((0,xF86XK_MonBrightnessDown), spawn "xbacklight -20")
                 -- Keyboard brightness
-                , ((0,xF86XK_KbdBrightnessUp), spawn "light -s sysfs/leds/smc::kbd_backlight -A 20")
-                , ((0,xF86XK_KbdBrightnessDown), spawn "light -s sysfs/leds/smc::kbd_backlight -U 20")
+                , ((0,xF86XK_KbdBrightnessUp), spawn "kbdlight up 20")
+                , ((0,xF86XK_KbdBrightnessDown), spawn "kbdlight down 20")
                 -- Media player controls
                 , ((0,xF86XK_AudioPlay), spawn "playerctl play-pause")
                 , ((0,xF86XK_AudioNext), spawn "playerctl next")
@@ -67,6 +68,9 @@ workspaceNames =
   ]
 
 myWorkspaces = workspaceNames
+
+myStartupHook = do
+    spawn "xmon up"
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey  myConfig
 
